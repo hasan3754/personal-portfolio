a3773ed451e7255b302be4d31f497eac15bf8856
mirazhasan47 <hasan3754@diu.edu.bd>
2023-07-28 14:59:29 +0600

    education section

--- src/components/EducationAndSkills/EducationAndSkills.jsx
+++ src/components/EducationAndSkills/EducationAndSkills.jsx
@@ -9,22 +9,21 @@ const EducationAndSkills = () => {
     <div>
     <div style={{width: "110%"}}>
       <Slide direction="left" duration={10000}>
-        <img style={{float: "right", width: "10%"}} src="./working.gif" alt="" />
+        <img style={{float: "right", width: "10%"}} src="./cycle.gif" alt="" />
       </Slide>
     </div>      
     <Slide direction="down">
-      <h1 className={css.margin} style={{ marginTop: 40 }}>My education and skills</h1>
+      <h1 className={css.margin} style={{ marginTop: 40, marginBottom: 150 }}>My education and skills</h1>
     </Slide>
     <div className="row" style={{ width: "80%", margin: "auto" }}>
-      <div className="col-6">
+      <div className="col-5">
       <div className={css.educationHeight}>
         <div class={css.block}>
           <div>
-            <h4>Degree : Bsc in Computer science and engineering</h4>
-            <h4>Institute : Daffodil international university</h4>
-            <h4>Duration : 2014-2019</h4>
+            <h5>Degree : Bsc in Computer science and engineering</h5>
+            <h5>Institute : Daffodil international university</h5>
+            <h5>Duration : 2014-2019</h5>
             <p>Achieving my bachelors degree was a great journey. I have practiced and learned computer science from my university. I solved competitive proramming problems and also done several projects course wise.</p>
-            <a href="https://daffodilvarsity.edu.bd/department/cse/program/1/1/undergraduate">Courses</a>
           </div>
         </div>
         <div class={css.sock}></div>
@@ -33,24 +32,25 @@ const EducationAndSkills = () => {
       <div className={css.educationHeight}>
         <div class={css.block}>
           <div>
-            <h4>Degree : Bsc in Computer science and engineering</h4>
-            <h4>Institute : Daffodil international university</h4>
-            <h4>Duration : 2014-2019</h4>
+            <h5>Degree : Bsc in Computer science and engineering</h5>
+            <h5>Institute : Daffodil international university</h5>
+            <h5>Duration : 2014-2019</h5>
             <p>Achieving my bachelors degree was a great journey. I have practiced and learned computer science from my university. I solved competitive proramming problems and also done several projects course wise.</p>
-            <a href="https://daffodilvarsity.edu.bd/department/cse/program/1/1/undergraduate">Courses</a>
           </div>
         </div>
         <div class={css.sock}></div>
         <div class={css.dock}></div>
       </div>
       </div>
-      <div className="col-6">
+      <div className="col-2">
+      </div>
+      <div className="col-5">
       <div class="row mb-5">
         <div class="col-12 col-sm-6 col-md-4">
           <div className={`${css.card} ${css.cardPurpleBlue} text-white mb-3 mb-md-0`}>
           <div className="card-body">
               <div className="row">
-                  <img className={css.imageSocial} style={{ width: 50, height: 50, margin: 'auto' }} src="./laravel.png" alt="" />
+                  <img className={css.imageSocial} style={{ maxWidth: 90, maxHeight: 50, margin: 'auto' }} src="./laravel.png" alt="" />
               </div>
               <div className="row">
                   <p style={{ margin: 'auto' }}>Laravel</p>
@@ -62,21 +62,23 @@ const EducationAndSkills = () => {
           <div className={`${css.card} ${css.cardGreenish} text-white mb-3 mb-md-0`}>
             <div className="card-body">
               <div className="row">
-                  <img className={css.imageSocial} style={{ width: 50, height: 50, margin: 'auto' }} src="./vue.png" alt="" />
+                  <img className={css.imageSocial} style={{ maxWidth: 90, maxHeight: 50, margin: 'auto' }} src="./vue.png" alt="" />
               </div>
               <div className="row">
-                  <p style={{ margin: 'auto' }}>HTML</p>
+                  <p style={{ margin: 'auto' }}>Vue</p>
               </div>
             </div>
           </div>
         </div>
         <div class="col-12 col-sm-6 col-md-4">
           <div className={`${css.card} ${css.cardPinkish} text-white mb-3 mb-md-0`}>
-            <div className="card-body d-flex justify-content-between align-items-end">
-              <div className="card-number">
-                <div className="h3 m-0">288K</div><small><strong>REVENUE</strong></small>
+          <div className="card-body">
+              <div className="row">
+                  <img className={css.imageSocial} style={{ maxWidth: 90, maxHeight: 50, margin: 'auto' }} src="./phplogo.png" alt="" />
               </div>
-              <div className="card-description text-right"><small>235K</small><br/><small>22.48%</small></div>
+              <div className="row">
+                  <p style={{ margin: 'auto' }}>PHP</p>
+              </div>
             </div>
           </div>
         </div>
@@ -84,31 +86,37 @@ const EducationAndSkills = () => {
         <div class="row mb-5">
         <div class="col-12 col-sm-6 col-md-4">
           <div className={`${css.card} ${css.cardBlueish} text-white mb-3 mb-md-0`}>
-            <div className="card-body d-flex justify-content-between align-items-end">
-              <div className="card-number">
-                <div className="h3 m-0">288K</div><small><strong>REVENUE</strong></small>
+          <div className="card-body">
+              <div className="row">
+                  <img className={css.imageSocial} style={{ maxWidth: 90, maxHeight: 50, margin: 'auto' }} src="./reactlogo.png" alt="" />
               </div>
-              <div className="card-description text-right"><small>235K</small><br/><small>22.48%</small></div>
+              <div className="row">
+                  <p style={{ margin: 'auto' }}>React</p>
+              </div>
             </div>
           </div>
         </div>
         <div class="col-12 col-sm-6 col-md-4">
-          <div className={`${css.card} ${css.cardRainbowish} text-white mb-3 mb-md-0`}>
-            <div className="card-body d-flex justify-content-between align-items-end">
-              <div className="card-number">
-                <div className="h3 m-0">288K</div><small><strong>REVENUE</strong></small>
+          <div className={`${css.card} ${css.cardBlueish} text-white mb-3 mb-md-0`}>
+          <div className="card-body">
+              <div className="row">
+                  <img className={css.imageSocial} style={{ maxWidth: 90, maxHeight: 50, margin: 'auto' }} src="./nodelogo.png" alt="" />
               </div>
-              <div className="card-description text-right"><small>235K</small><br/><small>22.48%</small></div>
+              <div className="row">
+                  <p style={{ margin: 'auto' }}>Node js</p>
+              </div>
             </div>
           </div>
         </div>
         <div class="col-12 col-sm-6 col-md-4">
           <div className={`${css.card} ${css.cardAnotherish} text-white mb-3 mb-md-0`}>
-            <div className="card-body d-flex justify-content-between align-items-end">
-              <div className="card-number">
-                <div className="h3 m-0">288K</div><small><strong>REVENUE</strong></small>
+          <div className="card-body">
+              <div className="row">
+                  <img className={css.imageSocial} style={{ maxWidth: 90, maxHeight: 50, margin: 'auto' }} src="./codeigniter.jpg" alt="" />
               </div>
-              <div className="card-description text-right"><small>235K</small><br/><small>22.48%</small></div>
+              <div className="row">
+                  <p style={{ margin: 'auto' }}>Codeigniter</p>
+              </div>
             </div>
           </div>
         </div>
